164e61d66f3aa286992cd13bc731df8efd9303e5
Valentin-KV <krivenko.vg@yandex.ru>
2024-04-28 19:19:28 +0300

    feat: add two cycle options

--- script.js
+++ script.js
@@ -63,6 +63,40 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
+// с помощью цикла while
+// let num = 0;
+// while (num < 2) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', '');
+
+// if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//   personalMovieDB.movies[a] = b;
+//   console.log('done');
+// } else {
+//   console.log('error');
+//   num--;
+// }
+// num++;
+// }
+
+// с помощью цикла do while
+
+// let num = 0;
+// do {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', '');
+
+// if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//   personalMovieDB.movies[a] = b;
+//   console.log('done');
+// } else {
+//   console.log('error');
+//   num--;
+// }
+// num++;
+// }
+// while (num < 2);
+
 if (personalMovieDB.count < 10) {
     console.log("Просмотрено довольно мало фильмов");
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
