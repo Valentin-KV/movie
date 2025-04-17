044f48cdb4fa452dde936c436ff142106506676f
Valentin-KV <krivenko.vg@yandex.ru>
2025-04-17 12:46:46 +0300

    feat: added check for film buffs

--- script.js
+++ script.js
@@ -62,7 +62,7 @@
 
 // Код возьмите из предыдущего домашнего задания
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -83,6 +83,16 @@ for (let i = 0; i < 2; i++) {
         console.log('error');
         i--;
     }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
 }
 
 console.log(personalMovieDB);
